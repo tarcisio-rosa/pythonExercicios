72c1f15902d2bfd7a5ae2e48416367835f9ff706
Tarcísio Rossi Rosa <tarcisio.rosa@outlook.com>
2022-09-11 10:47:07 -0300

    .

---
+++ seçao_06.11.py
@@ -0,0 +1,11 @@
+"""
+Faça um programa que leia um número inteiro positivo N e imprima todos os números
+naturais de 0 até N em ordem crescente
+"""
+
+num = int(input('Digite um número: '))
+x = 0
+
+while x <= num:
+    print(x)
+    x += 1
---
+++ seçao_06.12.py
@@ -0,0 +1,11 @@
+"""
+Faça um programa que leia um número inteiro positivo N e imprima todos os números
+naturais de 0 até N em ordem decrescente
+"""
+
+num = int(input('Digite um número: '))
+x = 0
+
+while num >= x:
+    print(num)
+    num -= 1
