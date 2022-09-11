2d72824b4fa9f58be5e1b4278f20243b4dbb406e
Tarcísio Rossi Rosa <tarcisio.rosa@outlook.com>
2022-09-11 10:56:07 -0300

    .

--- seçao_06.14.py
+++ seçao_06.14.py
@@ -1,14 +1,14 @@
 """
 Faça um programa que leia um número inteiro positivo par N e imprima todos os números
-naturais pares de 0 até N em ordem crescente
+naturais pares de 0 até N em ordem decrescente
 """
 
 num = int(input('Digite um número par: '))
 x = 0
 
 if num % 2 == 0:
-    while x <= num:
-        print(x)
-        x += 2
+    while num >= x:
+        print(num)
+        num -= 2
 else:
-    print('O número digitado não é par, tente novamente!')
+    print('Este número não é par, tente novamente!')
