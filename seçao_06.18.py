6db5af77df3ea65f2591fd55b450ca6dcf6932ff
Tarcísio Rossi Rosa <tarcisio.rosa@outlook.com>
2022-09-23 20:01:23 -0300

    resolução do exercício seção 06 número 14.

--- seçao_06.18.py
+++ seçao_06.18.py
@@ -3,9 +3,15 @@ Escreva um algoritmo que leia certa quantidade de números e imprima o maior del
 o maior número foi lido. A quantidade de números a serem lidos deve ser fornecida pelo usuário.
 """
 
-maior_numero: 0
+maior = 0
+continuar = None
+qtd = 0
 
-while True:
-    num = input('Digite um número: ')
-    if num >= maior_numero:
-        maior_numero = num
+while continuar != 'Não':
+    num = int(input('Digite um número: '))
+    if num > maior:
+        maior = num
+    if num == maior:
+            qtd += 1
+    continuar = input('Deseja continuar? ')
+print(f'O maior número digitado foi {maior}, ele foi digitado {qtd} vezes.')
